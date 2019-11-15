57405abbf3197d46de25942ddd025549a33dc869
TMc42 <tmclean1212@gmail.com>
2019-11-14 19:30:49 -0600

    Update dbfunctions.php
    added edit comment and edit post contents and category

--- dcsp_db/dbfuncs/dbfunctions.php
+++ dcsp_db/dbfuncs/dbfunctions.php
@@ -21,7 +21,14 @@ function db_add_message($connection, $un, $au, $ti, $msg){
     $result = $connection->query($insert_into_messages);
     if (!$result) die($connection->error);}
 
-// Use these functions to search for properties from the database dcsp_project_db
+function db_edit_post_category($connection, $pi, $ct){
+    $update_post = "UPDATE posts SET category = '$ct' WHERE post_id = '$pi'";
+    $result = $connection->query($update_post);    
+    if (!$result) die($connection->error);}
 
+function db_edit_comment($connection, $ci, $cn){
+    $update_comment = "UPDATE comments SET contents = '$cn' WHERE comment_id='$ci'";
+    $result = $connection->query($update_comment); 
+    if (!$result) die($connection->error);}
 
 ?>
