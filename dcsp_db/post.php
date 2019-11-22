99d2bd60585972ed4ab3a91d4633d9aca771ca30
Matthew Ashmore <cpu.nut98@gmail.com>
2019-11-22 10:45:02 -0600

    Commented post.php

--- dcsp_db/post.php
+++ dcsp_db/post.php
@@ -15,7 +15,7 @@
         session_start();
         require_once('dbfuncs/dbfunctions.php');
         require_once('dbfuncs/dblogin.php');
-
+        //Check if user is logged in, set vars as needed
         if(isset($_SESSION['currentUserType'])){
             if ($_SESSION['currentUserType'] == "admin"){
                 $loggedin = true;
@@ -50,6 +50,7 @@
   </head>
   <body style="background-color: #bfc9ca">
     <div class="container-fullwidth sticky-top">
+        <!--NavBar-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="main.php">
             <img src="stackunderflow.png" width="30" height="30" alt="">Stack Underflow
@@ -69,6 +70,7 @@
                 <a class="nav-link" href="inbox_page.php">Inbox</a>
             </li>
             <?php
+                //Change the account dropdown depending on the status of the user
                 if($loggedin && !$admin){
                     echo "<li class=\"nav-item dropdown\">
                     <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdownMenuLink\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
@@ -98,6 +100,7 @@
             ?>
             </ul>
         </div>
+        <!--Seach bar-->
         <form class="navbar-form navbar-right" method="get" action="search.php">
             <div class="input-group">
                 <input type="text" class="form-control" name="search_var" placeholder="Search Posts">
@@ -110,24 +113,29 @@
         </form>
         </nav>
     </div>
+    <!--Main-->
     <div class="container pt-5" style="background-color: #abb2b9">
         <div class="row">
             <div class="col-md-9">
                 <div class="container-fullwidth border border-dark border-3 p-3">
 
-                    <?php
+                    <?
+                        //Called if user tries to delete a comment
                         if(isset($_GET['deleteComment'])){
                             $postID = $_SESSION['postID'];
                             $commentID = $_GET['deleteComment'];
                             db_delete_comment($conn, $postID, $commentID);
+                            //Refresh the page
                             header("Location: post.php?post_id=$postID");
                             
                         }
+                        //Called if user tried to delete a post
                         if(isset($_GET['deletePost'])){
                             $postID = $_SESSION['postID'];
                             $queryDelComm = "SELECT * FROM comments WHERE post_id = '$postID'";
                             $resultDelComm = $conn->query($queryDelComm);
                             if($resultDelComm){
+                                //Delete every comment on the post first, then delete the post
                                 while($resultArrDelComm = $resultDelComm->fetch_array()){
                                     db_delete_comment($conn, $postID, $resultArrDelComm['comment_id']);
                                 }
@@ -135,6 +143,7 @@
                                 header("Location: main.php");
                             }
                         }
+                        //Called to handle input when user edits a comment
                         if(isset($_GET['submitCommEdit'])){
                             $postID = $_SESSION['postID'];
                             $commentID = $_GET['submitCommEdit'];
@@ -143,6 +152,7 @@
                             $contents = "";
                             if(isset($_GET['submitCommEdit'])){
                                 if(isset($_GET['commentcontents'])){
+                                    //Accept any text, but not only whitespace
                                     if(preg_match('/^(.*)$/ms',$_GET['commentcontents']) && !(ctype_space($_GET['commentcontents'])) && strlen($_GET['commentcontents']) > 5 && strlen($_GET['commentcontents']) <= 500){
                                         $commentErr = "";
                                         $commentErrBool = false;
@@ -151,6 +161,7 @@
                                         //ALL INFO IS GOOD, ADD THE POST AND FORWARD USER TO IT'S PAGE
                                         $contentsWithSlashes = addslashes($_GET['commentcontents']);
                                         db_edit_comment($conn, $commentID, $contentsWithSlashes);
+                                        //Refresh the page
                                         header("Location: post.php?post_id=$postID");
                                         
                                     } else {
@@ -161,6 +172,7 @@
                                 }
                             }
                         }
+                        //Called when user submits an edit to a post
                         if(isset($_GET['submitEdit'])){
                             $postID = $_SESSION['postID'];
                             $postErr = "";
@@ -168,6 +180,7 @@
                             $contents = "";
                             if(isset($_GET['submitEdit'])){
                                 if(isset($_GET['postcontents'])){
+                                    //Allow any text, not only whitespace
                                     if(preg_match('/^(.*)$/ms',$_GET['postcontents']) && !(ctype_space($_GET['postcontents'])) && strlen($_GET['postcontents']) > 5 && strlen($_GET['postcontents']) <= 500){
                                         $postErr = "";
                                         $postErrBool = false;
@@ -176,6 +189,7 @@
                                         //ALL INFO IS GOOD, ADD THE POST AND FORWARD USER TO IT'S PAGE
                                         $postContentsWithSlashes = addslashes($_GET['postcontents']);
                                         db_edit_contents($conn, $postID, $postContentsWithSlashes);
+                                        //Refresh the page
                                         header("Location: post.php?post_id=$postID");
                                         
                                     } else {
@@ -187,6 +201,7 @@
                                 }
                             }
                         }
+                        //Called when user clicks edit post
                         if(isset($_GET['editPost'])){
                             $postID = $_SESSION['postID'];
                             $query = "SELECT * FROM posts WHERE post_id = '$postID'";
@@ -194,6 +209,7 @@
                             if($result){
                                 $resultArr = $result->fetch_array();
                             }
+                            //Show a text area with the post contents inside to be edited
                             echo "<form action=\"post.php\" method=\"get\">
                                     <div class=\"row border border-dark border-3 rounded pt-3 pb-3\" style=\"background-color: #171717\">
                                     <div class=\"col-md-12\">
@@ -206,6 +222,7 @@
                                 <button type=\"submitEdit\" id=\"submitEdit\" name=\"submitEdit\" class=\"btn btn-primary\">Update Post</button>
                             </form>";
                         } else {
+                            //If the user has not clicked edit post, just show the post
                             if(!(isset($_SESSION['postID']))){
                                 $_SESSION['postID'] = $_GET['post_id'];
                             }
@@ -214,7 +231,7 @@
                             $result = $conn->query($query);
                             if($result){
                                 $resultArr = $result->fetch_array();
-                                
+                                //Display the post
                                 echo "<div class=\"row border border-dark border-3 rounded pt-3 pb-3\" style=\"background-color: #171717\">
                                 <div class=\"col-md-12\">
                                     <h3 class=\"dcsp-text-light\">" . $resultArr['post_title'] . "</h3>
@@ -236,14 +253,17 @@
                         $commentErr = "";
                         $commentErrBool = false;
                         $contents = "";
+                        //called if user submits a comment
                         if(isset($_GET['submit'])){
                             if(isset($_GET['commentcontents'])){
+                                //Allow all text, but not just whitespace alone.
                                 if(preg_match('/^(.*)$/ms',$_GET['commentcontents']) && !(ctype_space($_GET['commentcontents'])) && strlen($_GET['commentcontents']) > 5 && strlen($_GET['commentcontents']) <= 500){
                                     $commentErr = "";
                                     $commentErrBool = false;
 
                                     $time = time();
                                     $mysqltime = date("Y-m-d H:i:s", $phptime);
+                                    //Sanitize by adding backslashes to special chars, preventing issues with the SQL
                                     $commentContentsWithSlashes = addslashes($_GET['commentcontents']);
                                     db_add_comment($conn, $postID, $_SESSION['currentUser'], $commentContentsWithSlashes);
                                     $username = $_SESSION['currentUser'];
@@ -252,7 +272,6 @@
                                     header("Location: post.php?post_id=$postID");
                                     
                                 } else {
-
                                     $commentErr = "Your comment must be between 5 and 500 characters.";
                                     $commentErrBool = true;
                                     $contents = $_GET['commentcontents'];
@@ -266,7 +285,8 @@
                     ?>
                     <div class="container-fullwidth p-3">
                         <?php
-
+                            //If a user is not logged in, don't show the text box for comments.
+                            //Instead direct them to log in with a button
                             if(!$loggedin){
                                 echo "<div class=\"container text-center\">
                                 <h3>You must be logged in to comment!</h3>
@@ -291,12 +311,14 @@
                     </div>
 
                     <?php
+                        //Called when a user chooses to edit a comment
                         if(isset($_GET['editComment'])){
                             $postID = $_SESSION['postID'];
                             $commentID = $_GET['editComment'];
                             $queryComm = "SELECT * FROM comments WHERE comment_id = '$commentID'";
                             $resultComm = $conn->query($queryComm);
                             $resultArrComm = $resultComm->fetch_array();
+                            //Display form with text area allowing comment to be edited
                             echo "<div class=\"row border border-dark border-3 rounded pt-3 pb-3\" style=\"background-color: #171717\">
                                     <div class=\"col-md-9 text-truncate\">
                                         <h3 class=\"dcsp-text-light\">" . $resultArrComm['username'] . "</h3>
@@ -318,8 +340,7 @@
                                     <button type=\"submitCommEdit\" id=\"submitCommEdit\" name=\"submitCommEdit\" value=\"" . $commentID . "\" class=\"btn btn-primary\">Submit</button>
                                      </form></div></div>";
                         } else {
-
-
+                            //Otherwise, show all comments on the post
                             $queryComm = "SELECT * FROM comments WHERE post_id = '$postID' ORDER BY comment_id DESC";
                             $resultComm = $conn->query($queryComm);
                             if($resultComm){
@@ -330,6 +351,10 @@
                                         <h5 class=\"dcsp-text-light\">" . date("Y-M-d H:i:s", strtotime($resultArrComm['time']) - 6 * 3600 ) . "</h5>
                                     </div>";
                                     echo "<div class=\"col-md-3\">";   
+                                        //Check the comments to see if the user who is logged in wrote them
+                                        //If so, or they are an admin, show delete comment button
+                                        //If not an admin but still the person who made the comment, show edit comment button
+                                        //And is user is admin, but also wrote the post, show both as well
                                         if($loggedin){
                                             if($_SESSION['currentUser'] == $resultArrComm['username'] || $_SESSION['currentUserType'] == "admin"){
                                                 echo "<div class=\"row text-center\">
@@ -354,18 +379,20 @@
                     
                 </div>
             </div>
-
+            <!--Sidebar-->
             <div class="col-md-3">
+                <!--New Post Button-->
                 <div class="container-fullwidth border border-dark border-3 p-3 text-center">
                     <a href="createpost.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">New Post</a>
                 </div>
                 <div class="container-fullwidth border border-dark border-3 p-3 text-center">
+                    <!--Info for post. Who made it and when-->
                     <?php
                         $query = "SELECT * FROM posts WHERE post_id = '$postID'";
                         $result = $conn->query($query);
                         if($result){
                             $resultArr = $result->fetch_array();
-                            
+                            //Show post author and timestamp adjusted to UTC - 6h
                             echo "<div class=\"row border border-dark border-3 rounded pt-3 pb-3\" style=\"background-color: #171717\">
                             <div class=\"col-md-12\">
                                 <h3 class=\"dcsp-text-light\">Posted By:</h3>
@@ -373,7 +400,8 @@
                                 <h3 class=\"dcsp-text-light\">On:</h3>
                                 <h5 class=\"dcsp-text-light\">" . date("Y-M-d H:i:s", strtotime($resultArr['time']) - 6 * 3600 ) . "</h5>
                                 ";
-                            
+                            //If logged in and also same user that wrote post, show edit and delete buttons.
+                            //If admin, always show delete button regardless.
                             if($loggedin){
                                 if($_SESSION['currentUser'] == $resultArr['username'] || $_SESSION['currentUserType'] == "admin"){
                                     echo "<div class=\"container-fullwidth border border-dark border-3 p-3 text-center\">
